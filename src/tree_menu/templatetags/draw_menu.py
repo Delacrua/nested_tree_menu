1c6ddf360acf41b7f504581cbce0b4861eda760a
Uladzimir Fiodarau <ufiodarau@gmail.com>
2023-03-05 15:30:14 +0400

    Refactored draw_menu code a bit

--- src/tree_menu/templatetags/draw_menu.py
+++ src/tree_menu/templatetags/draw_menu.py
@@ -7,33 +7,30 @@ register = template.Library()
 
 @register.inclusion_tag("nested_menu.html", takes_context=True)
 def draw_menu(context, menu_name):
-    try:
-        items = Item.objects.select_related("menu").filter(menu__title=menu_name)
-        items_values = items.values()
-        items_listed = (list(items_values))
-        primary_items = [item for item in items_listed if item.get("parent_id") is None]
-        selected_item_id = int(context["request"].GET[menu_name])
-        items_id_dict = {item["id"]: item for item in items_listed}
-        selected_item_id_list = get_selected_item_id_list(items_id_dict, primary_items, selected_item_id)
 
-        for item in primary_items:
-            if item["id"] in selected_item_id_list:
-                item["child_items"] = get_child_items(items_values, item["id"], selected_item_id_list)
-        result_dict = {"items": primary_items}
+    items = Item.objects.select_related("menu").filter(menu__title=menu_name)
+    items_listed = (list(items.values()))
+    items_id_dict = {item["id"]: item for item in items_listed}
 
-    except:
-        filtered_items = Item.objects.select_related("menu").filter(menu__title=menu_name, parent=None)
-        result_dict = {"items": [item for item in filtered_items.values()]}
+    primary_items = [item for item in items_listed if item.get("parent_id") is None]
+    selected_item_id = int(context["request"].GET[menu_name])
+    selected_item_id_list = get_selected_item_id_list(items_id_dict, primary_items, selected_item_id)
 
+    for item in primary_items:
+        if item["id"] in selected_item_id_list:
+            item["child_items"] = get_child_items(items_listed, item["id"], selected_item_id_list)
+
+    result_dict = {"items": primary_items}
     result_dict["menu"] = menu_name
     result_dict["other_querystring"] = get_querystring(context, menu_name)
+
     return result_dict
 
 
-def get_querystring(context, menu):
+def get_querystring(context, menu_name):
     querystring_args = []
     for key in context["request"].GET:
-        if key != menu:
+        if key != menu_name:
             querystring_args.append(key + "=" + context["request"].GET[key])
     querystring = ("&").join(querystring_args)
     return querystring
